1d023920dcd54499d44d7fd0b5161de5464d0939
gbus <gianluca.busiello@gmail.com>
2013-06-10 22:55:08 +0100

    mv 3bot.drivers

--- 3go.py
+++ 3go.py
@@ -2,7 +2,7 @@
 
 import curses
 #from espeak import espeak
-from 3bot-drivers import Drive2
+from drivers-3bot import Drive2
 from servo import Servo
 from servo_config import channel_config
 import RPi.GPIO as GPIO
