02c389c74bba059dd5b05e52ead20fb442df4f84
Gianluca Busiello <busiello@ebi.ac.uk>
2013-10-14 16:02:50 +0100

    new sonarlib lib

---
+++ ranger/sonar.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>  
+#include <stdlib.h>  
+#include <unistd.h>  
+#include <signal.h>  
+#include <string.h>  
+#include <errno.h>  
+#include <sys/time.h>  
+#include <wiringPi.h>  
+#include "sonarlib.h"
+
+
+int main (int argc, char *argv[])  
+{  
+	int trig = 9;
+	int echo = 11;
+	Sonar* son = new Sonar(trig, echo);
+	int resp;
+	int i;
+
+	for (i = 0; i < 10; i++)  
+	{
+		resp = son.sonarread();
+		if ( resp == 0) {
+			printf("Distance : %lf mt\n", son.getdistance());
+		} else {
+			printf("Error %d\n", resp);
+		}		
+	}  
+}  
+
+
+
