4ca891b87d5b0198b0feca4b3dc060a192d63f6d
Gianluca Busiello <busiello@ebi.ac.uk>
2013-10-23 23:45:39 +0100

    f

--- servolib/main.cc
+++ servolib/main.cc
@@ -9,6 +9,7 @@
 #include <unistd.h>
 #include <stdint.h>
 #include <errno.h>
+#include <time.h>
 
 #define CHANNEL 0
 #define MIN	300
@@ -25,6 +26,7 @@ int main(void)
 	
 	cout << "Position reset to value " << s.reset_position() << endl;
 	cout << "Stepping +20 " << s.step_position(20) << endl;
+	sleep(3);
 	cout << "Position reset to value " << s.reset_position() << endl;
 	
 	exit(0);
