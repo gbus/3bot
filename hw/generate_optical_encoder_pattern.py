8e657243b335ceef84ee3a25da386e4401497aea
Gianluca Busiello (systems) <busiello@chianti.ebi.ac.uk>
2014-02-17 16:34:43 +0000

    optical encoder layout generator

--- hw/generate_optical_encoder_pattern.py
+++ hw/generate_optical_encoder_pattern.py
@@ -2,34 +2,40 @@ import pygame
 import sys
 import time
 
+pygame.init()
+
+# Convert measures in mm to pixels
 def getPixels(mm):
 	ppi = 72
 	im = 25.4	# 1 inch = 25.4 mm 
 	return int(round(mm*ppi/im))
 
-def drawbars(thickness, height, line):
+
+# Draw a sequence of bars along an horizontal line
+def drawbars(thickness, space, height, valign):
 	maxright = A4w - right_border
 	cursor = left_border
 	while cursor < maxright:
-		pygame.draw.rect(screen, black, (cursor,line, cursor+thickness, line+height), 0)
-		cursor = cursor + (2*thickness)
-		print (cursor)
+		pygame.draw.rect(screen, black, (cursor,valign, thickness, height), 0)
+		cursor = cursor + thickness + space
+		#print (thickness)
+		#print (cursor)
+
 
 A4w = getPixels(210)
 A4h = getPixels(297)
 black = [0,0,0]
 white = [255,255,255]
+gray = [100,100,100]
 circle = 175
 
-c0_5 = getPixels(circle/circle/2)
-c1 = getPixels(circle/circle)
-c1_5 = getPixels(circle/(circle+(circle/2)))
-c2 = getPixels(circle/2*circle)
-
+barsize = [ getPixels(0.5), getPixels(1), getPixels(1.5), getPixels(2), getPixels(2.5)]
 layout_width = getPixels(circle)
+
+
 bar_height = getPixels(15)
 bar_distance = getPixels(10)
-top_border = bottom_border = left_border = right_border = getPixels(20)
+top_border = bottom_border = left_border = right_border = getPixels(10)
 
 
 
@@ -39,11 +45,26 @@ screen = pygame.display.set_mode(size)
 
 screen.fill(white)
 
-# Print cut markers
-pygame.draw.line(screen, black, [left_border,top_border], [left_border, A4h-bottom_border])
-pygame.draw.line(screen, black, [left_border+layout_width,top_border], [left_border+layout_width, A4h-bottom_border])
+myfont = pygame.font.SysFont("monospace", 12)
 
-drawbars(c0_5, bar_height, 40)
+# Print cut markers
+pygame.draw.line(screen, gray, [left_border,top_border], [left_border, A4h-bottom_border])
+pygame.draw.line(screen, gray, [left_border+layout_width,top_border], [left_border+layout_width, A4h-bottom_border])
+
+
+# Draw the layout
+va = 0
+for count in range(2):
+	for bs in barsize:
+		no_bars = int (layout_width / bs / 2)
+		space = (layout_width-(bs*no_bars))/no_bars
+		va+=70
+		drawbars(bs, space, bar_height, va)
+		# Add a label to the bar line
+		label_text = "No. bars: "+str(no_bars)+" - Thickness(px): "+str(bs)+" - Spacing(px): "+str(space)
+		label = myfont.render(label_text , 1, (150,150,150))
+		screen.blit(label, (left_border, va+bar_height+3))
+
 pygame.display.update()
 
 time.sleep(3)
