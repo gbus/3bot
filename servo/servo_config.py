620fa53c42669c18754c9d13e92bb91a5b2aa518
Gianluca Busiello <gianluca.busiello@gmail.com>
2014-10-12 12:40:19 +0000

    Reviewed servo lib/web dir structure

---
+++ servo/servo_config.py
@@ -0,0 +1,32 @@
+# Servo config
+
+####
+# 0
+# 1
+# 2
+# ...
+# 11 pan 160-680
+# 12 tilt 220-480 (Range limited by camera shell)
+# ... 
+
+
+
+channel_config = { 
+0: {"minp": 300, "maxp": 500 },
+1: {"minp": 300, "maxp": 500 },
+2: {"minp": 300, "maxp": 400 },
+3: {"minp": 300, "maxp": 400 },
+4: {"minp": 180, "maxp": 500 },
+5: {"minp": 180, "maxp": 520 },
+6: {"minp": 300, "maxp": 400 },
+7: {"minp": 300, "maxp": 400 },
+8: {"minp": 300, "maxp": 400 },
+9: {"minp": 300, "maxp": 400 },
+10: {"minp": 300, "maxp": 400 },
+11: {"minp": 160, "maxp": 680 },
+12: {"minp": 220, "maxp": 480 },
+13: {"minp": 300, "maxp": 400 },
+14: {"minp": 300, "maxp": 400 },
+15: {"minp": 300, "maxp": 400 },
+}
+
